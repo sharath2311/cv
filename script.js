3d57a75c7e3482cc75a6fa77b70f8000f0ba1a4b
sharath2311 <sharathr54@gmail.com>
2020-06-23 18:06:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var a,b,c,d;
+a=78;
+b=1.69;
+c=92;
+d=1.95;
+
+var mB=a/(b*b);
+var jB=c/(d*d);
+if(mB>jB)
+{
+    var r=true;
+}
+console.log(mB,jB);
+console.log('mark bmi is higher than john?'+r);
